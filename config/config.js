078d2e9d6374a5c181240caead9910bc8d310d17
calininja <syg0713@gmail.com>
2022-04-15 17:03:50 +0900

    update: db and puppeteer addon

--- config/config.js
+++ config/config.js
@@ -2,30 +2,29 @@ const dotenv = require("dotenv");
 const product = process.env.NODE_ENV === "production";
 
 dotenv.config();
-
 module.exports = {
   development: {
-    username: product ? "bc2a164ec0ba33" : "root",
+    username: product ? "bb7098d217cb18" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_724a6f31bdaadac" : "calimemo",
+    database: product ? "heroku_d94c92834c35d19" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
     timezone: "+09:00",
   },
   test: {
-    username: product ? "bc2a164ec0ba33" : "root",
+    username: product ? "bb7098d217cb18" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_724a6f31bdaadac" : "calimemo",
+    database: product ? "heroku_d94c92834c35d19" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
     timezone: "+09:00",
   },
   production: {
-    username: product ? "bc2a164ec0ba33" : "root",
+    username: product ? "bb7098d217cb18" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_724a6f31bdaadac" : "calimemo",
+    database: product ? "heroku_d94c92834c35d19" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
