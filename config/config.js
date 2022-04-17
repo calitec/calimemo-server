88b0c3ab90c13ad845d8b7e0aa496f701c72fdac
calininja <syg0713@gmail.com>
2022-04-17 13:02:47 +0900

    update: db

--- config/config.js
+++ config/config.js
@@ -4,27 +4,27 @@ const product = process.env.NODE_ENV === "production";
 dotenv.config();
 module.exports = {
   development: {
-    username: product ? "bb7098d217cb18" : "root",
+    username: product ? "bf29f0d3b2bfb9" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_d94c92834c35d19" : "calimemo",
+    database: product ? "heroku_5033457910eb299" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
     timezone: "+09:00",
   },
   test: {
-    username: product ? "bb7098d217cb18" : "root",
+    username: product ? "bf29f0d3b2bfb9" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_d94c92834c35d19" : "calimemo",
+    database: product ? "heroku_5033457910eb299" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
     timezone: "+09:00",
   },
   production: {
-    username: product ? "bb7098d217cb18" : "root",
+    username: product ? "bf29f0d3b2bfb9" : "root",
     password: process.env.DB_PASSWORD,
-    database: product ? "heroku_d94c92834c35d19" : "calimemo",
+    database: product ? "heroku_5033457910eb299" : "calimemo",
     host: product ? "us-cdbr-east-05.cleardb.net" : "127.0.0.1",
     dialect: "mysql",
     dialectOptions: { charset: "utf8mb4", dateStrings: true, typeCast: true },
